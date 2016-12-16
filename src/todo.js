200c363c089c2d49fb63ac5846c5d44fe0a5472f
N-takumi <takumi0106n@gmail.com>
2016-12-16 22:44:10 +0900

    todoを配列で保存

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,8 @@
 function todolist(){
 
+  var APP_NAME = "Todolist_APP";//アプリ名
   var APP_VERSION = 0.1;//バージョン
-  var APP_NAME = "Todolist";//アプリ名
+
   var todolist_h = [];//todo保存用　配列
 
   function init(){
@@ -19,7 +20,7 @@ function todolist(){
     //Allclearボタンクリック時の処理
     $("#AllclearButton").click(
     function(){
-      TextClear();
+      clearText();
       showText();
     });
 
@@ -28,6 +29,7 @@ function todolist(){
   //Todoの読み込み及び表示
   function showText(){
 
+      //ローカルストレージからJSON形式→配列で取得
       todolist_h = JSON.parse(localStorage.getItem(APP_NAME));
 
       if(todolist_h){
@@ -46,31 +48,32 @@ function todolist(){
             //表示する前にエスケープ
             html.push($("<h5>").html("・"+escapeText(value)));
           }
-
           list.append(html);
         }
+      }else{
+        todolist_h = [];
       }
-
   }
 
   //テキストの保存
   function saveText(){
 
-
+    //ローカルストレージからJSON形式→配列で取得
     todolist_h = JSON.parse(localStorage.getItem(APP_NAME));
 
     if(todolist_h == null){
       todolist_h = [];
     }
 
-    var text = $("#formText");
+    var text = $("#formText");//フォームからテキスト取得
     var val = text.val();
 
-    todolist_h.push(val);
+
 
     //入力チェックを行う
     if(checkText(val)){
       //通ればセット
+      todolist_h.push(val);
       localStorage.setItem(APP_NAME,JSON.stringify(todolist_h));
     }
     //テキストボックスをからにする
@@ -78,33 +81,36 @@ function todolist(){
   }
 
   //Todoのクリア
-  function TextClear(){
-    localStorage.clear();
+  function clearText(){
+    todolist_h = [];//空配列で初期化
+    localStorage.setItem(APP_NAME,JSON.stringify(todolist_h));
   }
 
 
   //入力された文字をチェック
   function checkText(text){
 
+    //文字数チェック
     if(0 == text.length){
       alert("Todoが入力されていません")
       return false;
-    }else if(20 < text.length){
-      alert("文字数は20文字以下にしてください");
+    }else if(30 < text.length){
+      alert("文字数は30文字以下にしてください");
       return false;
     }
 
-    //すでに入力された値があれば不可//   書き換える必要がある 12/16-まだ
-    var len = localStorage.length;
-    for(var i = 0;i < len;i++){
-      var key = localStorage.key(i);
-      var value = localStorage.getItem(key);
-      //内容が一致するものがあるか比較
-      if(text == value){
-        alert("同じ内容のTodoが存在します");
-        return false;
+    //すでに入力された値があれば不可//   書き換える必要がある
+    var value;
+    if(todolist_h.length != 0){
+      for(var i = 0;i < todolist_h.length;i++){
+        value = todolist_h[i];
+        if(text == value){
+          alert("同じ内容のTodoが存在します");
+          return false;
+        }
       }
     }
+
 
     //全てのチェックを通過できれば可
     return true;
